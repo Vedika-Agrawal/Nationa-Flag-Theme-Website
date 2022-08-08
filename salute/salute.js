5765160cfd1ed505c25f87c788d25bdff5c8a31c
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2022-08-08 18:50:24 +0530

    Add files via upload

---
+++ salute/salute.js
@@ -0,0 +1,17 @@
+alert("Click on the flag 😉")
+function func()
+{
+alert("Happy 75 th Independence Day")
+alert("Listen to my favourite song too.")
+var music=document.getElementById("music");
+    music.play();
+    
+}alert("Click on the flag 😉")
+function func()
+{
+alert("Happy 75th Independence Day")
+alert("Listen to my favourite song too.")
+var music=document.getElementById("music");
+    music.play();
+    
+}
